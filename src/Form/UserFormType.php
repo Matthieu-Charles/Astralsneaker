46ef0c6ef83553b751e4148b00f7e6d7db293468
Tyson <nemeghaire.tyson@outlook.be>
2022-06-14 16:16:52 +0200

    Modification compte

--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -11,18 +11,44 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
 
 class UserFormType extends AbstractType
 {
+
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $user = $this->security->getUser();
+
+        if ($user) {
+            $builder
+                ->add('email', EmailType::class, [
+                    'label' => 'Email',
+                    'disabled' => true
+                ])
+                ->add('password', PasswordType::class, [
+                    'label' => 'Mot de passe',
+                    'required' => true
+                ]);
+        } else {
+            $builder
+                ->add('email', EmailType::class, [
+                    'label' => 'Email'
+                ])
+                ->add('password', PasswordType::class, [
+                    'label' => 'Mot de passe'
+                ]);
+        }
+
+
         $builder
-            ->add('email', EmailType::class, [
-                'label' => 'Email'
-            ])
-            ->add('password', PasswordType::class, [
-                'label' => 'Mot de passe'
-            ])
             ->add('lastName', TextType::class, [
                 'label' => 'Nom de famille'
             ])
